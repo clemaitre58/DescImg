b62d853b6c7b2a31716a39c70fa72c7dd9fa96fd
Cédric Lemaitre <c.lemaitre58@gmail.com>
2019-04-16 12:26:10 +0200

    modify git ignore for build and bin

--- build/CMakeFiles/main_coverage.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/main_coverage"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/main_coverage.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
